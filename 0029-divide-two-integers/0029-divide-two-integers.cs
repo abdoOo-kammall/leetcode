39aa69fbc53dd2ddcf0c8709eed69ea0f08b0926
abdulrahman kamal <65976632+abdoOo-kammall@users.noreply.github.com>
2025-05-30 21:50:20 +0300

    Time: 0 ms (100%), Space: 29.3 MB (32.82%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public int Divide(int dividend, int divisor) {
+        if (dividend == int.MinValue && divisor == -1 || dividend == int.MaxValue && divisor == 1   )
+            return int.MaxValue;
+        if (dividend == int.MinValue && divisor == 1   )
+            return int.MinValue  ;
+
+
+        bool flagD1 = false; 
+        bool flagD2 = false; 
+
+        if (divisor < 0) { divisor = -divisor; flagD2 = true; } 
+        if (dividend < 0) { dividend = -dividend; flagD1 = true; }
+       
+        long a = Math.Abs((long)dividend);
+        long b = Math.Abs((long)divisor);
+        int ans = 0;
+
+        while (a - b >= 0) {
+            int x = 0;
+            while (a - (b << (x + 1)) >= 0) {
+                x++;
+            }
+            ans += 1 << x;
+            a -= b << x;
+        }
+
+
+
+
+        return (flagD1 == flagD2) ? ans : -ans;
+    }
+}
