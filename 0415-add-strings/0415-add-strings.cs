1bfd9797d3e41fdf11ecf0d9b99330296eba8920
abdulrahman kamal <65976632+abdoOo-kammall@users.noreply.github.com>
2025-06-11 15:14:04 +0300

    Time: 1 ms (100%), Space: 43.1 MB (79.37%) - LeetHub

--- 0415-add-strings/0415-add-strings.cs
+++ 0415-add-strings/0415-add-strings.cs
@@ -1,9 +1,23 @@
-using System.Numerics;
-
 public class Solution {
     public string AddStrings(string num1, string num2) {
-        BigInteger x = BigInteger.Parse(num1);
-        BigInteger y = BigInteger.Parse(num2);
-        return (x + y).ToString();
+        int i = num1.Length - 1;
+        int j = num2.Length - 1;
+        int carry = 0;
+        List<char> result = new List<char>();
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int n1 = i >= 0 ? num1[i] - '0' : 0;
+            int n2 = j >= 0 ? num2[j] - '0' : 0;
+
+            int sum = n1 + n2 + carry;
+            result.Add((char)((sum % 10) + '0'));
+            carry = sum / 10;
+
+            i--;
+            j--;
+        }
+
+        result.Reverse();
+        return new string(result.ToArray());
     }
 }
