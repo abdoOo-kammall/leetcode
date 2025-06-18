95baeef67ccc589fdd8203a463605fa24594532f
abdulrahman kamal <65976632+abdoOo-kammall@users.noreply.github.com>
2025-06-18 13:56:10 +0300

    Time: 28 ms (32.45%), Space: 45.9 MB (41.5%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cs
+++ 0125-valid-palindrome/0125-valid-palindrome.cs
@@ -1,7 +1,8 @@
 using System.Text.RegularExpressions;
 public class Solution {
     public bool IsPalindrome(string s) {
-        string res =Regex.Replace(s, "[^a-zA-Z0-9]", "").ToLower();
+        string res =new String(s.Where(char.IsLetterOrDigit) .Select(char.ToLower)
+        .ToArray());
         for (int i = 0 , j = res.Length-1 ; i< j; i++,j--) {
             if (res[i] == res[j])continue;
             else  return false;
