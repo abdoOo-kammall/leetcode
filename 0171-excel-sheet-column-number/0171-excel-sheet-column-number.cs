3829dda9589bbb1d4cd55a460624f0ea7c7efb53
abdulrahman kamal <65976632+abdoOo-kammall@users.noreply.github.com>
2025-06-19 10:15:54 +0300

    Time: 0 ms (100%), Space: 41.1 MB (61.83%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cs
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cs
@@ -1,11 +1,9 @@
 public class Solution {
     public int TitleToNumber(string columnTitle) {
-        int res = 0;
-        double J = 0;
-        for (int i = columnTitle.Length - 1; i >= 0; i--) {
-            res += (int)(Math.Pow(26.0, J) * (columnTitle[i] - 'A' + 1));
-            J++;
+        int result = 0;
+        foreach (char c in columnTitle) {
+            result = result * 26 + (c - 'A' + 1);
         }
-        return res;
+        return result;
     }
 }
