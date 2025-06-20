807d114e5dd2d98fc6ba1980a059427b9471a29b
abdulrahman kamal <65976632+abdoOo-kammall@users.noreply.github.com>
2025-06-21 02:56:35 +0300

    Time: 0 ms (100%), Space: 43.7 MB (63.8%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cs
+++ 0242-valid-anagram/0242-valid-anagram.cs
@@ -1,19 +1,18 @@
 public class Solution {
     public bool IsAnagram(string s, string t) {
-        if (s.Length != t.Length ) return false ;
-        int[] arr1 = new int [27];
-        int[] arr2 = new int [27];
-        for(int i = 0 ; i<s.Length;i++) {
+        if (s.Length != t.Length) return false;
 
-            arr1[s[i] - 'a']++;
-        }
-        for(int i = 0 ; i<t.Length;i++) {
+        int[] counts = new int[26];  
 
-            arr2[t[i]-'a']++;
+        for (int i = 0; i < s.Length; i++) {
+            counts[s[i] - 'a']++;  
+            counts[t[i] - 'a']--;    
         }
-for (int i = 0 ; i<27 ; i++) {
-        if (arr1[i] != arr2[i]) return false;
-}
-return true;
+
+        foreach (int count in counts) {
+            if (count != 0) return false;  
+        }
+
+        return true;
     }
 }
