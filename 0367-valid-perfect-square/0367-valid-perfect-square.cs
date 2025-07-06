0bc6919c62b090c54ae7d122f28f7d8cff86c81c
abdulrahman kamal <65976632+abdoOo-kammall@users.noreply.github.com>
2025-07-06 13:52:47 +0300

    Time: 1 ms (18.45%), Space: 31.2 MB (17.71%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cs
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cs
@@ -1,10 +1,13 @@
 public class Solution {
     public bool IsPerfectSquare(int num) {
-        
-        for (int  i = 1 ; i<=100000 ; i++) 
-        {
-            if ((long) (i*i) == num) return true ;
-        }
-        return false ;
+        if (num < 1) return false;
+
+        double log10 = Math.Log10(num);
+        double logHalf = log10 / 2.0;
+
+        double root = Math.Pow(10, logHalf); // this should be the square root
+        int rounded = (int)Math.Round(root);
+
+        return rounded * rounded == num;
     }
 }
